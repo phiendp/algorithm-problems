79980e31e4702fb93c06cea95f23e40d5abfc5b1
Phien Pham <phiendpham@gmail.com>
2019-06-19 17:34:27 +0700

    Implement Max and Min in a Unsorted Array problem

--- unsorted_integer_array/max_min_unsorted_array.py
+++ unsorted_integer_array/max_min_unsorted_array.py
@@ -5,7 +5,16 @@ def get_min_max(ints):
     Args:
        ints(list): list of integers containing one or more integers
     """
-    pass
+    min_number = ints[0]
+    max_number = ints[0]
+    for num in ints:
+        if num > max_number:
+            max_number = num
+        if num < min_number:
+            min_number = num
+
+    return (min_number, max_number)
+
 
 ## Example Test Case of Ten Integers
 import random
@@ -14,3 +23,5 @@ l = [i for i in range(0, 10)]  # a list containing 0 - 9
 random.shuffle(l)
 
 print ("Pass" if ((0, 9) == get_min_max(l)) else "Fail")
+print("Pass" if ((0, 15) == get_min_max([15, 10, 6, 0, 11, 6])) else "Fail")
+print("Pass" if ((6, 20) == get_min_max([10, 15, 6, 20, 11, 6])) else "Fail")
